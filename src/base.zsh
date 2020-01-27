03fad9f56a7888e87c067a29bb142549f2d64636
@slovacus <slovacus@gmail.com>
2020-01-27 18:28:14 -0500

    (#2) chore: Implement base install dependences

--- src/base.zsh
+++ src/base.zsh
@@ -1,2 +1,30 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+function brew::exist {
+    if ! type -p brew > /dev/null; then
+        echo 0
+        return
+    fi
+    echo 1
+}
+
+function rsync::exist {
+    if ! type -p rsync > /dev/null; then
+        echo 0
+        return
+    fi
+    echo 1
+}
+
+function rsync::install {
+    if [ "$(brew::exist)" -eq 0 ]; then
+        message_warning "Please Install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+        return
+    fi
+    message_info "Installing Rsync"
+    brew install rsync
+    message_success "Installed Rsync"
+}
+
+if [ "$(rsync::exist)" -eq 0 ]; then rsync::install; fi
