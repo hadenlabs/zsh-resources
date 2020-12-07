bfe5d49be2dec59bc11e459450bc459b7eb468b9
Luis Mayta <luis@hadenlabs.com>
2020-12-07 00:56:26 -0500

    refactor: actions make (#32)

--- zsh-resources.zsh
+++ zsh-resources.zsh
@@ -8,47 +8,13 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-# shellcheck disable=SC2034  # Unused variables left for readability
-RESOURCES_ROOT_DIR=$(dirname "${0}")
-RESOURCES_SRC_DIR="${RESOURCES_ROOT_DIR}"/pkg
-RESOURCES_ASSETS_DIR="${RESOURCES_ROOT_DIR}"/assets
-RESOURCES_ASSETS_FONTS_DIR="${RESOURCES_ASSETS_DIR}"/fonts
-RESOURCES_ASYNC_NAME="resources_worker"
-
-export RESOURCES_PACKAGE_NAME='resources'
-
-function resources::path::linux {
-    # shellcheck source=/dev/null
-    source "${RESOURCES_SRC_DIR}"/linux.zsh
-}
-
-function resources::path::osx {
-    # shellcheck source=/dev/null
-    source "${RESOURCES_SRC_DIR}"/osx.zsh
-}
-
-function resources::path::factory {
-    case "${OSTYPE}" in
-        darwin*)
-            resources::path::osx
-            ;;
-        linux*)
-            resources::path::linux
-            ;;
-    esac
-}
-
+ZSH_RESOURCES_PATH=$(dirname "${0}")
 
 # shellcheck source=/dev/null
-source "${RESOURCES_SRC_DIR}"/base.zsh
+source "${ZSH_RESOURCES_PATH}"/config/main.zsh
 
 # shellcheck source=/dev/null
-source "${RESOURCES_SRC_DIR}"/utils.zsh
+source "${ZSH_RESOURCES_PATH}"/internal/main.zsh
 
 # shellcheck source=/dev/null
-source "${RESOURCES_SRC_DIR}"/async.zsh
-
-resources::path::factory
-
-# shellcheck source=/dev/null
-source "${RESOURCES_SRC_DIR}"/fonts.zsh
+source "${ZSH_RESOURCES_PATH}"/pkg/main.zsh
