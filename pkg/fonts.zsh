ac34658d5406ae36043b3c20db1c994bc15d4c8f
@slovacus <slovacus@gmail.com>
2020-12-07 00:57:11 -0500

    refactor: resources function upgrade structure (#32)

--- pkg/fonts.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ksh
-# -*- coding: utf-8 -*-
-
-function resources::fonts::sync {
-    rsync -avzh --progress "${RESOURCES_ASSETS_FONTS_DIR}/" "${RESOURCES_FONTS_DIR}/"
-    if type -p fc-cache > /dev/null; then
-        fc-cache -f "${RESOURCES_FONTS_DIR}"
-    fi
-}
